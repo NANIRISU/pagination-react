cb33541792f6d0b69fd9d63afa23eab45d3edc28
NANIRISU <39872073+NANIRISU@users.noreply.github.com>
2021-05-04 21:22:56 +0530

    added pagination

--- src/App.js
+++ src/App.js
@@ -18,14 +18,14 @@ export default function App() {
     apiCallFunction();
   }, [pageNumber]);
 
-  // const MAX_PAGE_NUMBER=20
-  // const pageNumberArray=
+  const MAX_PAGE_NUMBER = 20;
+  const pageNumberArray = new Array(20).fill(1).map((_, idx) => idx + 1);
   const handlePre = () => {
     setPageNumber(pageNumber <= 1 ? pageNumber : pageNumber - 1);
   };
   const handleNext = () => {
-    setPageNumber(pageNumber + 1);
-    // setPageNumber(pageNumber >= 20 ? pageNumber : pageNumber + 1);
+    // setPageNumber(pageNumber + 1);
+    setPageNumber(pageNumber >= 20 ? pageNumber : pageNumber + 1);
   };
   return (
     <div className="App">
@@ -40,6 +40,13 @@ export default function App() {
       <button disabled={pageNumber === 20} onClick={handleNext}>
         Next
       </button>
+      <div>
+        {pageNumberArray.map((item, idx) => (
+          <button key={idx} onClick={() => setPageNumber(idx + 1)}>
+            {item}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
